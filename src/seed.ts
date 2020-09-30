4b7f637a68583618470ffb9b76137b3454936fd2
freedomsean <t101598009@ntut.org.tw>
2020-09-30 23:17:52 +0800

    chore: add seed script

---
+++ src/seed.ts
@@ -0,0 +1,17 @@
+import { TestingLib } from './test/testing.lib';
+
+const args = process.argv.slice(2);
+
+if (args[0] === 'up') {
+  (async () => {
+    await TestingLib.connectToDB();
+    await TestingLib.createTestKanban();
+    await TestingLib.closeDBConnection();
+  })();
+} else {
+  (async () => {
+    await TestingLib.connectToDB();
+    await TestingLib.deleteTestUser();
+    await TestingLib.closeDBConnection();
+  })();
+}
